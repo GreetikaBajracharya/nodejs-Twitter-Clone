d9f33f4f6f98f2b67f80fe5099924b945010ef4c
Greetika <bajracharyagreetika1@gmail.com>
2025-01-02 20:53:37 +0545

    1-notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import {v2 as cloudinary} from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -29,13 +30,10 @@ console.log(process.env.MONGO_URI);
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
+app.use("/api/notifications",notificationRoutes);
 
 app.listen(PORT,() => {
-    console.log("Server running on: "+PORT);
+    console.log(`Server running on: ${PORT}`);
     connectMongoDB();
 })
 
-
-//2:42
-
-//4:00:20
